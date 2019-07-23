f36cb951dba579904a6ea0479d494fe452ce0621
Matthew Hough <matthough1@icloud.com>
2019-07-22 23:29:45 -0400

    remove unused code

--- init.py
+++ init.py
@@ -1,6 +1,4 @@
-import os
-from flask import Flask
-from model import Post, User, Message
+from model import User
 from app import app, db
 
 
@@ -8,15 +6,9 @@ def initialize(db):
     with app.app_context():
         db.create_all()
         john = User(username='johndoe')
-        post = Post()
-        post.title = "Hello World"
-        post.body = "This is the first post"
-        post.author = john
-        db.session.add(post)
         db.session.add(john)
         db.session.commit()
         User.query.all()
-        Post.query.all()
 
 
 initialize(db)
--- model.py
+++ model.py
@@ -15,18 +15,6 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
-class Post(db.Model):
-    __tablename__ = 'posts'
-    uuid = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(256), index=True)
-    body = db.Column(db.Text)
-    author_id = db.Column(
-        db.Integer, db.ForeignKey('users.uuid'))
-
-    def __repr__(self):
-        return '<Post %r>' % self.title
-
-
 class Message(db.Model):
     __tablename__ = 'messages'
     uuid = db.Column(db.Integer, primary_key=True)
